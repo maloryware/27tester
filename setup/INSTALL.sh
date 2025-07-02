cfabab870f8ed82b5a6415e4863250525a9a7cf3
Mal <malory@onenetbeyond.org>
2025-07-02 13:40:15 +0100

    rectified shebang hopefully

--- setup/INSTALL.sh
+++ setup/INSTALL.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-$DIR27 = ~/.local/27tester
-$EXEC27 = ~/.local/bin/27tester
+DIR27=~/.local/27tester
+EXEC27=~/.local/bin/27tester
 
 
 # Clone the repository into ~/.local/27tester (hopefully outside of $PATH)
 mkdir -p $DIR27
 cd $DIR27
-@git init
-@git remote add origin https://github.com/maloryware/27tester
-@git pull
+git init
+git remote add origin https://github.com/maloryware/27tester
+git branch --set-upstream-to=origin/main main
+git pull origin main
 
 # Setup the '27tester' shell script, within path (~/.local/bin/27tester)
+touch $EXEC27
+curl https://codeberg.org/maloryware/27tester/raw/branch/main/setup/27tester > $EXEC27
